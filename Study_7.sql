c304fbd736b780c093b5ecb12b1bd8f7f52129cb
Mehmet Emin Aydin <95590673+mehmet-emin-aydin@users.noreply.github.com>
2022-08-20 04:02:13 -0700

    Add files via upload

---
+++ Study_7.sql
@@ -0,0 +1,4 @@
+-- SELECT rating, COUNT(*) FROM film GROUP BY rating ORDER BY COUNT(*);
+-- SELECT replacement_cost, COUNT(*) FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50 ORDER BY COUNT(*); 
+-- SELECT store_id, COUNT(*) FROM customer GROUP BY store_id ORDER BY COUNT(*);
+-- SELECT country_id, COUNT(*) FROM city GROUP BY country_id ORDER BY COUNT(*) DESC LIMIT 1;
